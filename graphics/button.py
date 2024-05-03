ec9c7978b15cd328c989e57f3be189e536c75572
nukestarmaster <nukestarmaster@gmail.com>
2024-05-03 10:13:52 -0700

    Got text displaying in the centre of buttons.

--- graphics/button.py
+++ graphics/button.py
@@ -1,14 +1,15 @@
 import pygame
 
-
 class Button():
-    def __init__(self, colour, colour_pressed, x, y, h, w, active = False, parent = None, text = '', button_event = None):
+    def __init__(self, colour, colour_pressed, x, y, h, w, font, active = False, parent = None, text = '', button_event = None):
         self.colour = colour
         self.colour_pressed = colour_pressed
         self.rect = pygame.Rect(x, y, w, h)
+        self.font = font
         self.active = active
         self.parent = parent
         self.text = text
+        self.text_image = self.font.render(self.text, 1, 'black')
         self.button_event = button_event
         if self.parent is not None:
             parent.add_child(self)
@@ -20,6 +21,7 @@ class Button():
         if pressed:
             colour = self.colour_pressed
         pygame.draw.rect(win, colour, self.rect, 0)
+        win.blit(self.text_image, self.text_image.get_rect(center = self.rect.center))
 
     def click(self):
         self.button_event()
--- graphics/window.py
+++ graphics/window.py
@@ -5,16 +5,19 @@ from sys import exit
 width = 800
 height = 600
 framerate = 60
-test_button2 = Button('blue', 'red', 200, 300, 100, 200, button_event= lambda : print('clicked'))
-test_button = Button('red', 'blue', 50, 100, 100, 200, True, button_event= test_button2.activate)
-buttons = [test_button, test_button2]
 
 pygame.init()
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Untitled Dragon Game')
+
+
 clock = pygame.time.Clock()
 test_surface = pygame.Surface((400, 300))
 test_surface.fill('Red')
+game_font = pygame.font.Font(None, 36)
+test_button2 = Button('blue', 'red', 200, 300, 100, 200, game_font, text = "hello again", button_event= lambda : print('clicked'))
+test_button = Button('red', 'blue', 50, 100, 100, 200, game_font, True, text= "hello", button_event= test_button2.activate)
+buttons = [test_button, test_button2]
 
 while True:
     for event in pygame.event.get():
