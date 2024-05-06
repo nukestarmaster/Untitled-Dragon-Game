fb7fcdeedd33f56c952a3203a5a2317c9a8003fd
nukestarmaster <69655738+nukestarmaster@users.noreply.github.com>
2024-05-06 08:35:29 -0700

    Implemented all button colour logic.

--- graphics/button.py
+++ graphics/button.py
@@ -4,8 +4,11 @@ class Button():
     def __init__(self, colour_theme, x, y, h, w, font, active = False, parent = None, text = '', button_event = None):
         self.colour_theme = colour_theme
         self.rect = pygame.Rect(x, y, w, h)
+        self.border = pygame.Rect(x - 2, y - 2, w + 4, h + 4)
         self.font = font
         self.active = active
+        self.hovered = False
+        self.clicked = False
         self.parent = parent
         self.text = text
         self.text_image = self.font.render(self.text, 1, self.colour_theme.text)
@@ -13,12 +16,15 @@ class Button():
         if self.parent is not None:
             parent.add_child(self)
 
-    def draw(self, win, pressed = False):
+    def draw(self, win):
         if not self.active:
             return
         colour = self.colour_theme.active
-        if pressed:
-            colour = self.colour_theme.clicked
+        if self.clicked:
+            colour = self.colour_theme.click
+        elif self.hovered:
+            colour = self.colour_theme.hover
+        pygame.draw.rect(win, self.colour_theme.border, self.border, 0)
         pygame.draw.rect(win, colour, self.rect, 0)
         win.blit(self.text_image, self.text_image.get_rect(center = self.rect.center))
 
--- graphics/window.py
+++ graphics/window.py
@@ -6,33 +6,50 @@ from sys import exit
 width = 800
 height = 600
 framerate = 60
-red_theme = Button_Theme('red', 'grey', 'burgandy', 'black', 'black', 'dark grey')
+red_theme = Button_Theme('red', 'grey', (131, 0, 0), 'black', 'black', 'dark grey')
+blue_theme = Button_Theme('blue', 'grey', (0, 0, 131), 'black', 'black', 'dark grey')
 
 pygame.init()
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Untitled Dragon Game')
-
-
 clock = pygame.time.Clock()
+
+
 test_surface = pygame.Surface((400, 300))
 test_surface.fill('Red')
 game_font = pygame.font.Font(None, 36)
 test_button2 = Button(red_theme, 200, 300, 100, 200, game_font, text = "hello again", button_event= lambda : print('clicked'))
-test_button = Button(red_theme, 50, 100, 100, 200, game_font, True, text= "hello", button_event= test_button2.activate)
+test_button = Button(blue_theme, 50, 100, 100, 200, game_font, True, text= "hello", button_event= test_button2.activate)
 buttons = [test_button, test_button2]
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-        if event.type == pygame.MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
-            for b in buttons:
-                if b.active and b.rect.collidepoint(pos):
-                    b.button_event()
-    screen.fill("dark grey")
-    for b in buttons:
-        b.draw(screen)
-    pygame.display.update()
-    clock.tick(framerate)
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                for b in buttons:
+                    if b.active and b.rect.collidepoint(pos):
+                        b.clicked = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                for b in buttons:
+                    b.clicked = False
+                    if b.active and b.rect.collidepoint(pos):
+                        b.button_event()
+            if event.type == pygame.MOUSEMOTION:
+                pos = pygame.mouse.get_pos()
+                for b in buttons:
+                    if b.active and b.rect.collidepoint(pos):
+                        b.hovered = True
+                    else:
+                        b.hovered = False
+        screen.fill("dark grey")
+        for b in buttons:
+            b.draw(screen)
+        pygame.display.update()
+        clock.tick(framerate)
+
+main()
